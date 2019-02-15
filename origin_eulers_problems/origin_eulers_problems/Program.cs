a546401fdbfeaf998ca1e00abfabe5a3f160ead0
William Carpenter-Frank <G17C0339@ict.ru.ac.za>
2019-02-15 12:23:37 +0200

    William/g17c0339 has done Eulers problem #1

--- origin_eulers_problems/origin_eulers_problems/Program.cs
+++ origin_eulers_problems/origin_eulers_problems/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace origin_eulers_problems
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int sumTotal = 0;
+            int i = 0;
+
+            while (i < 1000)
+            {
+                if (i %3==0  || i %5==0 )
+                {
+                    sumTotal+=i;
+                }
+                i++;                
+            }
+            Console.WriteLine(sumTotal);
+
         }
     }
 }
