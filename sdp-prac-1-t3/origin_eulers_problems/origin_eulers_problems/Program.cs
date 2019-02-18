f49061c0e50563c6ac3db63acc1346beef3f3a7b
Matthew <MDPitcairn@gmail.com>
2019-02-18 06:28:49 +0200

    Adding Matthew's code

---
+++ sdp-prac-1-t3/origin_eulers_problems/origin_eulers_problems/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace origin_eulers_problems
+{
+    class Program
+    {
+        static long MaxFactor(long num)
+        {
+            long k = 2;
+            while (k * k <= num)
+            {
+                if (num % k == 0)
+                {
+                    num /= k;
+                }
+                else
+                {
+                    ++k;
+                }
+            }
+
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{MaxFactor(600851475143)} is the largest prime number of 600851475143");
+            Console.ReadLine();
+        }
+    }
+}
