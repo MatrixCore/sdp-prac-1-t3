b5ae04a8334ca7c1102921221dbdbb54da0cf018
Matthew P <31068187+MatrixCore@users.noreply.github.com>
2019-02-18 07:12:21 +0200

    Actually fixing code for solution #3

--- origin_eulers_problems/origin_eulers_problems/Program.cs
+++ origin_eulers_problems/origin_eulers_problems/Program.cs
@@ -8,8 +8,28 @@ namespace origin_eulers_problems
 {
     class Program
     {
+        static long MaxFactor(long num)
+        {
+            long k = 2;
+            while (k * k <= num)
+            {
+                if (num % k == 0)
+                {
+                    num /= k;
+                }
+                else
+                {
+                    ++k;
+                }
+            }
+
+            return num;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine($"{MaxFactor(600851475143)} is the largest prime number of 600851475143");
+            Console.ReadLine();
         }
     }
 }
